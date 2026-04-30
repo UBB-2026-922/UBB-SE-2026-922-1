b49db24d31aa1210d383855df6b05c616d4bcb46
Campean Tudor <tudorv.camepean@gmail.com>
2026-05-01 01:21:09 +0300

    Fix SA1412: re-encode modified files as UTF-8 with BOM

--- BankingApp/src/BankingApp.Infrastructure/DataAccess/AppDatabaseContext.cs
+++ BankingApp/src/BankingApp.Infrastructure/DataAccess/AppDatabaseContext.cs
@@ -1,4 +1,4 @@
-// <copyright file="AppDatabaseContext.cs" company="CtrlC CtrlV">
+﻿// <copyright file="AppDatabaseContext.cs" company="CtrlC CtrlV">
 // Copyright (c) CtrlC CtrlV. All rights reserved.
 // </copyright>
 // <summary>
--- BankingApp/src/BankingApp.Infrastructure/DataAccess/Implementations/NotificationPreferenceDataAccess.cs
+++ BankingApp/src/BankingApp.Infrastructure/DataAccess/Implementations/NotificationPreferenceDataAccess.cs
@@ -1,4 +1,4 @@
-// <copyright file="NotificationPreferenceDataAccess.cs" company="CtrlC CtrlV">
+﻿// <copyright file="NotificationPreferenceDataAccess.cs" company="CtrlC CtrlV">
 // Copyright (c) CtrlC CtrlV. All rights reserved.
 // </copyright>
 // <summary>
