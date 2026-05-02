75c337f05ccb4e7c832f92e218d3df2fe73c7aa9
DinaBogdan912 <bogdan.dina@stud.ubbcluj.ro>
2026-05-02 15:04:31 +0300

    Added Beneficiary model (changed IBAN to Iban as in first project)

--- BankingApp/src/BankingApp.Domain/Entities/Beneficiary.cs
+++ BankingApp/src/BankingApp.Domain/Entities/Beneficiary.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿// <copyright file="Beneficiary.cs" company="UBB-922">
 // Copyright (c) UBB-922. All rights reserved.
 // </copyright>
@@ -21,6 +22,26 @@ public class Beneficiary
 {
     /// <summary>
     ///     Gets or sets the unique identifier for this beneficiary.
+=======
+// <copyright file="Beneficiary.cs" company="CtrlC CtrlV">
+// Copyright (c) CtrlC CtrlV. All rights reserved.
+// </copyright>
+// <summary>
+// Contains the Beneficiary class.
+// </summary>
+
+using System;
+
+namespace BankingApp.Domain.Entities;
+
+/// <summary>
+///     Represents a beneficiary to whom a user can send money.
+/// </summary>
+public class Beneficiary
+{
+    /// <summary>
+    ///     Gets or sets the unique identifier for the beneficiary.
+>>>>>>> ad1f882 (Added Beneficiary model (changed IBAN to Iban as in first project))
     /// </summary>
     /// <value>
     ///     Gets or sets the current value.
@@ -28,7 +49,11 @@ public class Beneficiary
     public int Id { get; set; }
 
     /// <summary>
+<<<<<<< HEAD
     ///     Gets or sets the identifier of the <see cref="User" /> who owns this beneficiary entry.
+=======
+    ///     Gets or sets the identifier of the user who owns this beneficiary.
+>>>>>>> ad1f882 (Added Beneficiary model (changed IBAN to Iban as in first project))
     /// </summary>
     /// <value>
     ///     Gets or sets the current value.
@@ -36,7 +61,11 @@ public class Beneficiary
     public int UserId { get; set; }
 
     /// <summary>
+<<<<<<< HEAD
     ///     Gets or sets the full display name of the beneficiary.
+=======
+    ///     Gets or sets the name of the beneficiary.
+>>>>>>> ad1f882 (Added Beneficiary model (changed IBAN to Iban as in first project))
     /// </summary>
     /// <value>
     ///     Gets or sets the current value.
@@ -52,7 +81,11 @@ public class Beneficiary
     public string Iban { get; set; } = string.Empty;
 
     /// <summary>
+<<<<<<< HEAD
     ///     Gets or sets the name of the beneficiary's bank, if known.
+=======
+    ///     Gets or sets the name of the beneficiary's bank.
+>>>>>>> ad1f882 (Added Beneficiary model (changed IBAN to Iban as in first project))
     /// </summary>
     /// <value>
     ///     Gets or sets the current value.
@@ -60,8 +93,12 @@ public class Beneficiary
     public string? BankName { get; set; }
 
     /// <summary>
+<<<<<<< HEAD
     ///     Gets or sets the date and time of the most recent transfer to this beneficiary,
     ///     or <see langword="null" /> if no transfer has been made yet.
+=======
+    ///     Gets or sets the date and time of the last transfer made to this beneficiary.
+>>>>>>> ad1f882 (Added Beneficiary model (changed IBAN to Iban as in first project))
     /// </summary>
     /// <value>
     ///     Gets or sets the current value.
@@ -69,7 +106,11 @@ public class Beneficiary
     public DateTime? LastTransferDate { get; set; }
 
     /// <summary>
+<<<<<<< HEAD
     ///     Gets or sets the cumulative amount sent to this beneficiary across all transfers.
+=======
+    ///     Gets or sets the total amount of money sent to this beneficiary.
+>>>>>>> ad1f882 (Added Beneficiary model (changed IBAN to Iban as in first project))
     /// </summary>
     /// <value>
     ///     Gets or sets the current value.
@@ -85,10 +126,18 @@ public class Beneficiary
     public int TransferCount { get; set; }
 
     /// <summary>
+<<<<<<< HEAD
     ///     Gets or sets the date and time (UTC) when this beneficiary was created.
+=======
+    ///     Gets or sets the date and time when the beneficiary was created.
+>>>>>>> ad1f882 (Added Beneficiary model (changed IBAN to Iban as in first project))
     /// </summary>
     /// <value>
     ///     Gets or sets the current value.
     /// </value>
     public DateTime CreatedAt { get; set; }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> ad1f882 (Added Beneficiary model (changed IBAN to Iban as in first project))
