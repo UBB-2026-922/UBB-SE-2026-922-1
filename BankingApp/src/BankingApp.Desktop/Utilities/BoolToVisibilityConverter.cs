9d2a5c2b29c0d24ab39be3557f2e9764371b5a51
DavidCostiug912 <david.costiug@stud.ubbcluj.ro>
2026-05-04 20:21:28 +0300

    feat: add transfer converters and TransferViewModel

---
+++ BankingApp/src/BankingApp.Desktop/Utilities/BoolToVisibilityConverter.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="BoolToVisibilityConverter.cs" company="CtrlC CtrlV">
+// Copyright (c) CtrlC CtrlV. All rights reserved.
+// </copyright>
+// <summary>
+// Contains the BoolToVisibilityConverter class.
+// </summary>
+
+using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+
+namespace BankingApp.Desktop.Utilities;
+
+/// <summary>
+///     Maps <see langword="true" /> to <see cref="Visibility.Visible" /> and
+///     <see langword="false" /> (or non-bool) to <see cref="Visibility.Collapsed" />.
+/// </summary>
+public class BoolToVisibilityConverter : IValueConverter
+{
+    /// <summary>
+    ///     Converts a boolean value to <see cref="Visibility" />.
+    /// </summary>
+    /// <param name="value">The boolean value to convert.</param>
+    /// <param name="targetType">The target type (unused).</param>
+    /// <param name="parameter">The parameter (unused).</param>
+    /// <param name="language">The language (unused).</param>
+    /// <returns><see cref="Visibility.Visible" /> when <paramref name="value" /> is true; otherwise <see cref="Visibility.Collapsed" />.</returns>
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is bool isVisible)
+        {
+            return isVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        return Visibility.Collapsed;
+    }
+
+    /// <summary>
+    ///     Converts a <see cref="Visibility" /> value back to a boolean.
+    /// </summary>
+    /// <param name="value">The visibility value.</param>
+    /// <param name="targetType">The target type (unused).</param>
+    /// <param name="parameter">The parameter (unused).</param>
+    /// <param name="language">The language (unused).</param>
+    /// <returns><see langword="true" /> when <paramref name="value" /> is <see cref="Visibility.Visible" />.</returns>
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        return value is Visibility visibility && visibility == Visibility.Visible;
+    }
+}
