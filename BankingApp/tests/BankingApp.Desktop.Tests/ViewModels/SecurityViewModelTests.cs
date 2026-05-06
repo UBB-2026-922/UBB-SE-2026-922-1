3e9bd063eef54c0fd3760907f0fc17c56de813ac
Campean Tudor <tudorv.camepean@gmail.com>
2026-05-06 12:39:04 +0300

    test(desktop): add initial setup for SecurityViewModel tests

---
+++ BankingApp/tests/BankingApp.Desktop.Tests/ViewModels/SecurityViewModelTests.cs
@@ -0,0 +1,32 @@
+// <copyright file="SecurityViewModelTests.cs" company="UBB-922">
+// Copyright (c) UBB-922. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankingApp.Application.DataTransferObjects.Profile;
+using BankingApp.Desktop.Enums;
+using BankingApp.Desktop.Utilities;
+using BankingApp.Desktop.ViewModels;
+using ErrorOr;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace BankingApp.Desktop.Tests.ViewModels;
+
+public class SecurityViewModelTests
+{
+    private readonly Mock<IApiClient> _mockApiClient;
+    private readonly Mock<ILogger<SecurityViewModel>> _mockLogger;
+    private readonly SecurityViewModel _viewModel;
+
+    public SecurityViewModelTests()
+    {
+        _mockApiClient = new Mock<IApiClient>();
+        _mockLogger = new Mock<ILogger<SecurityViewModel>>();
+        _viewModel = new SecurityViewModel(_mockApiClient.Object, _mockLogger.Object);
+    }
+}
