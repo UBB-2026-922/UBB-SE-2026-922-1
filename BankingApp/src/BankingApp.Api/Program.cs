2262f62ff6cea59be031e819e3949ff019a00e1c
Rares Bogdan <bogdan.andrei.rares@gmail.com>
2026-05-24 12:51:37 -0700

    refactor(api): replace MediatR ISender with direct service injection in all controllers

--- BankingApp/src/BankingApp.Api/Program.cs
+++ BankingApp/src/BankingApp.Api/Program.cs
@@ -1,14 +1,13 @@
 ﻿using System.Globalization;
 using BankingApp.Api.Middleware;
 using BankingApp.Application.DependencyInjection;
-using BankingApp.Application.Features.UserRegistration.Commands;
+using BankingApp.Application.Features.Authentication.Services;
 using BankingApp.Contracts.Http;
 using BankingApp.Domain.Common.Errors;
 using BankingApp.Infrastructure.Core.DependencyInjection;
 using BankingApp.Infrastructure.Persistence.Data;
 using BankingApp.Infrastructure.Persistence.DependencyInjection;
 using ErrorOr;
-using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi;
 using Serilog;
@@ -146,8 +145,8 @@ static async Task SeedDevelopmentLoginAsync(WebApplication application)
     }
 
     using IServiceScope scope = application.Services.CreateScope();
-    ISender sender = scope.ServiceProvider.GetRequiredService<ISender>();
-    ErrorOr<Success> result = await sender.Send(new RegisterCommand(email, password, fullName));
+    IAuthService authService = scope.ServiceProvider.GetRequiredService<IAuthService>();
+    ErrorOr<Success> result = await authService.RegisterAsync(email, password, fullName);
 
     if (!result.IsError)
     {
